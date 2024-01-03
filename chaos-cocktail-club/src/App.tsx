0db7d424659b6169aed988ec27c9589a08584244
Birte Bea Haack <BHaack@fette-compacting.com>
2024-01-03 15:45:13 +0100

    create nav bar and a list of espresso martini with responsive design

--- chaos-cocktail-club/src/App.tsx
+++ chaos-cocktail-club/src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./styles/style.css";
+
+import Cocktails from "./components/cocktails";
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="martini_glass.png"
+              alt="logo"
+              width="25"
+              height="25"
+              className="d-inline-block align-top"
+            />
+            CCC
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#cocktails">Cocktails</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <NavDropdown title="Categories" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#categories/new">New</NavDropdown.Item>
+                <NavDropdown.Item href="#categories/classic">
+                  Classic
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#categories/fancy">
+                  Fancy
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#categories/oftheday">
+                  Of the day
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* Add more Nav.Link components as needed */}
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-dark">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="container">
+        <div className="row">
+          <div className="col-md col-sm-6">
+            <Cocktails />
+          </div>
+          <div className="col-md col-sm-6">
+            <Cocktails />
+          </div>
+          <div className="col-md col-sm-6">
+            <Cocktails />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
